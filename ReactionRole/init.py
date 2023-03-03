0fdc7a5a1f7ae29468264613c45a1c1de537d8f4
Ted <84779771+Ted-18@users.noreply.github.com>
2023-03-03 06:51:00 +0100

    1.0.0

---
+++ ReactionRole/init.py
@@ -0,0 +1,32 @@
+# Github informations
+enableGithub = True
+author = "Ted-18"
+repository = "ReactionRole"
+version = "1.0.0"
+
+
+# To activate this addon
+cogEnabled = False
+
+# Name of the addon
+cogName = "reactionrole"
+
+# Name of the file containing the cog
+cogFile = "cogReactionRole"
+
+# List of packages required by the addon
+packageDependencies = [
+    "py-cord",
+    "mysql-connector-python"
+]
+
+# List of addons required by the addon
+addonDependencies = []
+
+# List of permissions required by the addon
+addonPermissions = [
+    "manage_roles",
+    "send_messages",
+    "add_reactions",
+    "read_message_history"
+]
